f7ceb90cf99a637ef144b00fe676c8f6b6458bdb
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-10-08 12:05:40 -0600

    Feat: Cree la tarjeta sin estilos de songCard y estoy intentando darle logica a la aplicacion en el mainMusic para mostrar albumes de musica de forma predeterminada

--- src/components/pages/Home/components/busqueda/busqueda.tsx
+++ src/components/pages/Home/components/busqueda/busqueda.tsx
@@ -12,7 +12,6 @@ export default function Busqueda() {
         e.preventDefault();
         searchSongs(query);
       }  
-
     }
 
     return (
--- src/components/pages/Home/components/mainMusic/mainMusic.tsx
+++ src/components/pages/Home/components/mainMusic/mainMusic.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
 import "./mainMusi.css";
+import axios from 'axios';
 
 export default function Home(){
     
+    const [albums, setAlbums] = useState([]);
+
+    useEffect(() => {
+        const fetchAlbums = async () => {
+            try {
+                const response = await axios.get('https://api.deezer.com/chart/0/albums?limit=10')
+                setAlbums(response.data.data);
+            } catch(error){
+                console.error(error)
+            }
+        };
+
+        fetchAlbums();
+    }, []);
+
     return(
         <div className="home__container">
-            
+            {/* Aqui pondre el componente SongCard*/}
         </div>
     )
 
