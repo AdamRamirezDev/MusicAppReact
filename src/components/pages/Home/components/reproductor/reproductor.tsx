db516f564f04fa3b091d4fa8c0f510021422bf67
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-10-11 12:28:55 -0600

    STYLES: Modifique estilos basicos del reproductor, el lunes continuare con el desarrollo

--- src/components/pages/Home/components/reproductor/reproductor.tsx
+++ src/components/pages/Home/components/reproductor/reproductor.tsx
@@ -45,7 +45,9 @@ export default function Reproductor({ currentTrack }: ReproductorProps) {
         </div>
       </div>
       <div className="reproductor__container__controles">
-        <button onClick={togglePlay}>{isPlaying ? "PAUSA" : "PLAY"}</button>
+        <button onClick={togglePlay}>{isPlaying ? "PAUSA" : 
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#fffefe" fill-rule="evenodd" d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2S2 6.477 2 12s4.477 10 10 10m-1.306-6.154l4.72-2.787c.781-.462.781-1.656 0-2.118l-4.72-2.787C9.934 7.706 9 8.29 9 9.214v5.573c0 .923.934 1.507 1.694 1.059" clip-rule="evenodd"/></svg> }
+          </button>
       </div>
       <audio ref={audioRef} />
       <div className="reproductor__container__volumen">
